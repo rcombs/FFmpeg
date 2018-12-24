0afc9e6b6b03627e16b680dd893b5adc18ff5ccc
Clément Bœsch <u@pkh.me>
2018-12-24 15:15:52 +0100

    lavc/decode: merge two video logic blocks

--- libavcodec/decode.c
+++ libavcodec/decode.c
@@ -409,6 +409,8 @@ static inline int decode_simple_internal(AVCodecContext *avctx, AVFrame *frame, 
     if (avctx->codec->type == AVMEDIA_TYPE_VIDEO) {
         if (frame->flags & AV_FRAME_FLAG_DISCARD)
             got_frame = 0;
+        if (ret >= 0 && !(avctx->flags & AV_CODEC_FLAG_TRUNCATED))
+            ret = pkt->size;
     } else if (avctx->codec->type == AVMEDIA_TYPE_AUDIO) {
         uint8_t *side;
         buffer_size_t side_size;
@@ -522,9 +524,6 @@ FF_ENABLE_DEPRECATION_WARNINGS
 
     if (!got_frame)
         av_frame_unref(frame);
-
-    if (ret >= 0 && avctx->codec->type == AVMEDIA_TYPE_VIDEO && !(avctx->flags & AV_CODEC_FLAG_TRUNCATED))
-        ret = pkt->size;
 
 #if FF_API_AVCTX_TIMEBASE
     if (avctx->framerate.num > 0 && avctx->framerate.den > 0)
